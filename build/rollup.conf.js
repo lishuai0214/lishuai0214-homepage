26c5ef606b6b1f7fc7304e2df6bc72a1ddb0cb7c
lishuai0214 <lishuai0214@gmail.com>
2018-08-13 23:48:02 +0800

    update

---
+++ build/rollup.conf.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const path = require('path')
+const vue = require('rollup-plugin-vue')
+const babel = require('rollup-plugin-babel')
+const resolve = require('rollup-plugin-node-resolve')
+
+const base = path.resolve(__dirname, '..')
+const src = path.resolve(base, 'src')
+const dist = path.resolve(base, 'dist')
+const {name} = require('../package.json')
+
+// Ensure dist directory exists
+if (!fs.existsSync(dist)) {
+  fs.mkdirSync(dist)
+}
+
+module.exports = {
+  input: path.resolve(src, 'index.js'),
+  external: ['vue'],
+  plugins: [
+    vue(),
+    resolve({external: ['vue']}),
+    babel({
+      babelrc: false,
+      "presets": [
+        [
+          "env",
+          {
+            "modules": false,
+            "targets": {
+              "browsers": [
+                "> 1%",
+                "last 2 versions",
+                "not ie <= 8"
+              ]
+            }
+          }
+        ],
+        "stage-2"
+      ],
+      plugins: [
+        'transform-object-assign',
+        'external-helpers'
+      ]
+    })
+  ],
+  output: [
+    {
+      format: 'cjs',
+      file: path.resolve(dist, `${name}.common.js`),
+      sourcemap: true
+    },
+    {
+      format: 'es',
+      file: path.resolve(dist, `${name}.esm.js`),
+      sourcemap: true
+    }
+  ]
+}
---
+++ docs/components/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <section>
+    <div class="jumbotron">
+      <logo></logo>
+      <br/>
+      <br/>
+      <p>
+        <a href="https://travis-ci.org/wxsms/uiv">
+          <img src="https://travis-ci.org/wxsms/uiv.svg?branch=master" alt="Build Status">
+        </a>
+        <a href="https://coveralls.io/github/wxsms/uiv?branch=master">
+          <img src="https://coveralls.io/repos/github/wxsms/uiv/badge.svg?branch=master" alt="Coverage Status">
+        </a>
+        <a href="https://www.npmjs.com/package/uiv">
+          <img src="https://img.shields.io/npm/dm/uiv.svg" alt="NPM Downloads">
+        </a>
+        <a href="https://www.npmjs.com/package/uiv">
+          <img src="https://img.shields.io/npm/v/uiv.svg" alt="NPM Version">
+        </a>
+        <a href="https://cdnjs.com/libraries/uiv">
+          <img src="https://img.shields.io/cdnjs/v/uiv.svg" alt="CDNJS Version">
+        </a>
+        <a href="https://github.com/wxsms/uiv">
+          <img src="https://img.shields.io/github/license/wxsms/uiv.svg" alt="License">
+        </a>
+      </p>
+      <h1 class="page-header">uiv</h1>
+      <h3><b>Bootstrap 3</b> Components implemented by <b>Vue 2</b>.</h3>
+      <br/>
+      <div>
+        <btn to="/dist-docs/getting-started" size="lg">Getting Started</btn>
+        <btn href="https://github.com/wxsms/uiv" size="lg">Github</btn>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import Logo from './architecture/Logo.vue'
+
+  export default {
+    components: {Logo}
+  }
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+  @import "../assets/css/variables";
+
+  section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+
+    .jumbotron {
+      background: transparent;
+
+      .btn {
+        background: transparent;
+        color: #fff;
+        transition: all .3s ease-in-out;
+        border: 1px solid #fff;
+        margin-right: 10px;
+
+        &:visited, &:focus, &:active {
+          color: #fff;
+          border: 1px solid #fff;
+        }
+
+        &:hover {
+          background: #fff;
+          color: #333;
+          border: 1px solid #fff;
+        }
+      }
+    }
+  }
+</style>
